65509318b626089a61b1a6d321f7c6ed5831980e
Filip Wojciechowski <fwojciec@gmail.com>
2022-04-24 16:03:48 -0700

    nvim keymap improvements

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -2,11 +2,11 @@ vim.opt_local.expandtab = false
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 
-vim.api.nvim_set_keymap("n", "<leader>t", ":TestNearest -race<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>T", ":TestFile -race<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>ts", ":TestSuite -race<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>tl", ":TestLast -race<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>tv", ":TestVisit -race<CR>", {})
+vim.keymap.set("n", "<leader>t", ":TestNearest -race<CR>")
+vim.keymap.set("n", "<leader>T", ":TestFile -race<CR>")
+vim.keymap.set("n", "<leader>ts", ":TestSuite -race<CR>")
+vim.keymap.set("n", "<leader>tl", ":TestLast -race<CR>")
+vim.keymap.set("n", "<leader>tv", ":TestVisit -race<CR>")
 
 vim.b.go_highlight_trailing_whitespace_error = 0
 vim.b.go_highlight_string_spellcheck = 1
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -6,50 +6,50 @@ local telescope_builtin = require("telescope.builtin")
 local telescope_user = require("user.telescope")
 vim.keymap.set("n", "<leader><leader>", function()
 	telescope_user.project_files()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>ff", function()
 	telescope_user.project_files()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader><CR>", function()
 	telescope_builtin.buffers()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>fb", function()
 	telescope_builtin.buffers()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>fb", function()
 	telescope_builtin.buffers()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>fd", function()
 	telescope_builtin.diagnostics()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>fh", function()
 	telescope_builtin.help_tags()
-end, { noremap = true, silent = true })
+end)
 
 vim.keymap.set("n", "<leader>fg", function()
 	telescope_builtin.live_grep()
-end, { noremap = true, silent = true })
+end)
 
 -- editing
-vim.keymap.set("v", "p", '"_dP', { noremap = true })
-vim.keymap.set("v", "Y", "y$", { noremap = true })
+vim.keymap.set("v", "p", '"_dP')
+vim.keymap.set("v", "Y", "y$")
 
 -- navigation
-vim.keymap.set("c", "<C-k>", "<Up>", { noremap = true })
-vim.keymap.set("c", "<C-j>", "<Down>", { noremap = true })
+vim.keymap.set("c", "<C-k>", "<Up>")
+vim.keymap.set("c", "<C-j>", "<Down>")
 
 -- vim test
-vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
-vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
-vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", {})
-vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
-vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>", {})
+vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
+vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
+vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
+vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
+vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
 
 -- treesitter fix
-vim.keymap.set("n", "<leader>we", ":write | edit | TSBufEnable highlight<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>we", ":write | edit | TSBufEnable highlight<CR>")
