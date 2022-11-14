3369ea01f21ea7d89b1e2602592ae0a64ed9d1fa
Filip Wojciechowski <fwojciec@gmail.com>
2022-11-14 12:57:08 -0800

    nvim: add ctrl k j mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,6 +69,8 @@ local function xnoremap(lhs, rhs, buffer)
   vim.keymap.set("x", lhs, rhs, { noremap = true, silent = true, buffer = buffer })
 end
 
+vim.keymap.set("c", "<C-k>", "<Up>")
+vim.keymap.set("c", "<C-j>", "<Down>")
 nnoremap("[d", vim.diagnostic.goto_prev)
 nnoremap("]d", vim.diagnostic.goto_next)
 nnoremap("<leader>d", vim.diagnostic.open_float)
