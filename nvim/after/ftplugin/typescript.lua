20e5e28620511e00774d778f88c1e0066f1977b1
Filip Wojciechowski <fwojciec@gmail.com>
2022-11-23 08:25:21 -0800

    nvim: deno support improvements

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -1,14 +1,12 @@
-local is_deno = vim.fn.glob("./deno.json*") ~= ""
+local is_ts = vim.fn.glob("./deno.json*") == ""
 
-if is_deno then
-  return
+if is_ts then
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = vim.api.nvim_create_augroup("TypeScriptGroup", { clear = true }),
+    callback = function()
+      local bufnr = vim.fn.bufnr()
+      local file_name = vim.api.nvim_buf_get_name(bufnr)
+      require("fw.utils").run_format_cmd({ "prettier", "--stdin-filepath", file_name })
+    end
+  })
 end
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("TypeScriptGroup", { clear = true }),
-  callback = function()
-    local bufnr = vim.fn.bufnr()
-    local file_name = vim.api.nvim_buf_get_name(bufnr)
-    require("fw.utils").run_format_cmd({ "prettier", "--stdin-filepath", file_name })
-  end
-})
--- nvim/lua/fw/treesitter.lua
+++ nvim/lua/fw/treesitter.lua
@@ -8,7 +8,7 @@ require("nvim-treesitter.configs").setup({
     -- disable = { "lua", "typescript", "go" },
     -- additional_vim_regex_highlighting = false,
   },
-  -- indent = { enable = false, disable = {} },
+  indent = { enable = true, disable = {} },
   -- context_commentstring = {
   --   enable = true,
   --   enable_autocmd = false,
