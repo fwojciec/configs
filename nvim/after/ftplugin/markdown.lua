db61ea87b673fae9181a427e8353325b7b1fde55
Filip Wojciechowski <fwojciec@gmail.com>
2022-12-27 13:00:06 -0800

    refine ftplugin autocommands

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,6 +2,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("MarkdownGroup", { clear = true }),
   buffer = 0,
   callback = function()
-    require("fw.utils").run_format_cmd({ "prettier", "--stdin-filepath", "<afile>" })
+    local bufnr = vim.fn.bufnr()
+    local file_name = vim.api.nvim_buf_get_name(bufnr)
+    require("fw.utils").run_format_cmd({ "prettier", "--stdin-filepath", file_name })
   end
 })
--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -1,6 +1,6 @@
 vim.b.indent_blankline_enabled = 1
 
-local yamlGrp = vim.api.nvim_create_augroup("GoGroup", { clear = true })
+local yamlGrp = vim.api.nvim_create_augroup("YamlGroup", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = yamlGrp,
   buffer = 0,
