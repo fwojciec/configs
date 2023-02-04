4b6682a172bdc32a898079686f884a0e3b15cb16
Filip Wojciechowski <fwojciec@gmail.com>
2023-02-04 09:40:28 -0800

    nvim: header files are c not cpp

--- nvim/lua/fw/autocommands.lua
+++ nvim/lua/fw/autocommands.lua
@@ -1,16 +1,25 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function() vim.highlight.on_yank({ timeout = 40 }) end,
-  group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 40 })
+	end,
+	group = vim.api.nvim_create_augroup("Yank", { clear = true }),
 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*",
-  command = "FormatWrite"
+	pattern = "*",
+	command = "FormatWrite",
 })
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufRead" }, {
-  pattern = "*",
-  command = [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]],
-  group = vim.api.nvim_create_augroup("CursorPositionGroup", { clear = true }),
-  once = true
+	pattern = "*",
+	command = [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]],
+	group = vim.api.nvim_create_augroup("CursorPositionGroup", { clear = true }),
+	once = true,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.h,*.c",
+	callback = function()
+		vim.bo.filetype = "c"
+	end,
 })
