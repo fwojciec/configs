25ea6d519189523594d2e624f4f0759e17e6ee51
Filip Wojciechowski <fwojciec@gmail.com>
2023-02-13 09:32:58 -0800

    nvim: update lua ls name

--- nvim/lua/fw/lsp.lua
+++ nvim/lua/fw/lsp.lua
@@ -54,7 +54,7 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
 	on_attach = custom_on_attach,
 	capabilities = capabilities,
 	settings = {
