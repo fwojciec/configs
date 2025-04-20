b681d8a6964fbff5c64634b232ada4f903a008e4
Filip Wojciechowski <fwojciec@gmail.com>
2025-04-19 20:29:06 -0700

    update config

--- nvim/lua/fw/lazy.lua
+++ nvim/lua/fw/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		"--branch=stable",
 		lazypath,
 	})
 end
@@ -22,142 +22,79 @@ require("lazy").setup({
 		event = { "BufReadPre", "BufNewFile" },
 		build = ":TSUpdate",
 	},
-
 	{
-		"hrsh7th/nvim-cmp",
+		"saghen/blink.cmp",
 		event = "InsertEnter",
+		version = "1.*",
 		dependencies = {
-			{ "hrsh7th/cmp-buffer", event = "InsertEnter" },
-			{ "hrsh7th/cmp-path", event = "InsertEnter" },
-			{ "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
-			{ "hrsh7th/cmp-nvim-lua", event = "InsertEnter" },
-			{ "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
+			"rafamadriz/friendly-snippets",
+		},
+		opts = {
+			keymap = { preset = "default" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				menu = { border = "single" },
+				documentation = { window = { border = "single" }, auto_show = false },
+			},
+			fuzzy = {
+				implementation = "prefer_rust_with_warning",
+			},
 		},
 	},
-
-	-- == LSP ==
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			{ "folke/neodev.nvim", config = true },
 		},
-		config = function()
-			-- LSP setup goes here (attach servers, keymaps)
-			-- Example: require('lspconfig').gopls.setup{}
-		end,
 	},
-
-	-- == UI / UX ==
 	{
 		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy", -- Load after startup
+		event = "VeryLazy",
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		event = { "BufReadPre", "BufNewFile" }, -- Load early for visual consistency
-		main = "ibl", -- Specify the main module for better lazy-loading
-		opts = {
-			-- Indent-blankline options
-			-- scope = { enabled = false }, -- Example
-		},
-	},
-	{
-		"OXY2DEV/markview.nvim",
-		ft = { "markdown", "codecompanion" }, -- Load only for markdown or codecompanion buffers
-		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Ensure Treesitter is loaded
-		opts = {
-			preview = {
-				filetypes = { "markdown", "codecompanion" }, -- Redundant due to `ft` trigger, but harmless
-				ignore_buftypes = {},
-			},
-			-- Add other markview options if needed
-		},
+		event = { "BufReadPre", "BufNewFile" },
+		main = "ibl",
 	},
 
-	-- == Themes == (Load early, set priority)
 	{
 		"sainnhe/gruvbox-material",
 		priority = 1000,
-		config = function() --[[ vim.cmd.colorscheme "gruvbox-material" ]]
-		end,
 	},
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		config = function() --[[ vim.cmd.colorscheme "tokyonight" ]]
-		end,
-	},
-	{
-		"romainl/Apprentice",
-		priority = 1000,
-		config = function() --[[ vim.cmd.colorscheme "apprentice" ]]
-		end,
-	},
-	-- Add a line here after lazy.setup to set your desired colorscheme, e.g.:
-	-- vim.cmd.colorscheme "tokyonight"
 
-	-- == Telescope ==
 	{
 		"nvim-telescope/telescope.nvim",
-		cmd = "Telescope", -- Load when :Telescope is called
+		cmd = "Telescope",
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- Already listed above, lazy.nvim handles it
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
-		config = function()
-			-- Telescope setup and keymaps go here
-			-- Example: require('telescope').setup { ... }
-		end,
 	},
 
-	-- == Tim Pope Classics (Generally load on demand) ==
-	{ "tpope/vim-commentary", event = "VeryLazy" }, -- Or use `keys` if you map `gc` etc.
-	{ "tpope/vim-surround", event = "VeryLazy" }, -- Needs setup or keymaps to trigger loading if using `keys`
-	{ "tpope/vim-repeat", event = "VeryLazy" }, -- Often used with surround/commentary
-	{ "tpope/vim-unimpaired", event = "VeryLazy" }, -- Or use `keys` for its mappings
-	{ "tpope/vim-fugitive", cmd = { "Git", "G" } }, -- Load on Git commands
-	{ "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" }, event = "VeryLazy" }, -- Enhances fugitive
-	{ "tpope/vim-abolish", cmd = { "Abolish", "Subvert" }, event = "VeryLazy" }, -- Load on commands or lazily
+	{ "tpope/vim-commentary", event = "VeryLazy" },
+	{ "tpope/vim-surround", event = "VeryLazy" },
+	{ "tpope/vim-repeat", event = "VeryLazy" },
+	{ "tpope/vim-unimpaired", event = "VeryLazy" },
+	{ "tpope/vim-fugitive", cmd = { "Git", "G" } },
+	{ "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" }, event = "VeryLazy" },
+	{ "tpope/vim-abolish", cmd = { "Abolish", "Subvert" }, event = "VeryLazy" },
 
-	-- == Development / Utility ==
-	{ "vim-test/vim-test", cmd = { "TestNearest", "TestFile", "TestSuite" } }, -- Load on test commands
+	{ "vim-test/vim-test", cmd = { "TestNearest", "TestFile", "TestSuite" } },
 	{
 		"mhartington/formatter.nvim",
-		event = "BufWritePre", -- Example: Load when saving a buffer to format
-		-- Or use cmd = "Format" if you format manually
-		config = function()
-			-- Formatter setup goes here
-			-- Example: require('formatter').setup { ... }
-		end,
+		event = "BufWritePre",
 	},
-	{ "christianrondeau/vim-base64", cmd = { "Base64Encode", "Base64Decode" } }, -- Load on command
-
-	-- == AI / Assistance ==
-	{ "github/copilot.vim", event = "VeryLazy" }, -- Load after startup
+	{ "github/copilot.vim", event = "VeryLazy" },
 	{
 		"olimorris/codecompanion.nvim",
-		-- Using the specific branch as in original config
-		branch = "feat/move-to-function-calling",
-		cmd = { "CodeCompanion" }, -- Load when CodeCompanion command is used
-		-- Dependencies are implicitly handled by lazy.nvim (plenary, treesitter)
-		-- Explicitly listing them is fine too if needed for clarity or specific dep config
+		cmd = { "CodeCompanion" },
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-		opts = {}, -- Keep existing opts
-		-- config = function() ... end -- Add config if needed
 	},
 
-	-- == Language Specific ==
-	{ "towolf/vim-helm", ft = "helm" }, -- Load for Helm files
-	{ "cappyzawa/starlark.vim", ft = "starlark" }, -- Load for Starlark files
-	{ "mrjosh/helm-ls", ft = "helm" }, -- Load for Helm files (likely related to LSP/helm)
-	{ "barreiroleo/ltex_extra.nvim", ft = { "markdown", "tex", "plaintex" } }, -- Load for text/markup files
-
-	-- == Commented Out / Unused ==
-	-- "rafamadriz/friendly-snippets", -- Now potentially a dependency of LuaSnip
-	-- "ahmedkhalf/project.nvim",
+	{ "towolf/vim-helm", ft = "helm" },
+	{ "cappyzawa/starlark.vim", ft = "starlark" },
+	{ "barreiroleo/ltex_extra.nvim", ft = { "markdown", "tex", "plaintex" } },
 })
-
--- Set your preferred colorscheme AFTER lazy setup
--- This ensures the theme plugin has been loaded (due to priority=1000)
--- vim.cmd.colorscheme "tokyonight" -- Example, uncomment and choose one
--- nvim/lua/fw/lsp.lua
+++ nvim/lua/fw/lsp.lua
@@ -33,7 +33,9 @@ local function custom_on_attach(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+-- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+
 
 -- Make runtime files discoverable to the server
 -- local runtime_path = vim.split(package.path, ";")
